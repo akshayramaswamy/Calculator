5c593faf9602d648c66bb4be24324859bec4230c
Akshay Ramaswamy <akshayramaswamy1@gmail.com>
2017-01-18 00:59:55 -0800

    calculator display almost working

--- Calculator/CalculatorBrain.swift
+++ Calculator/CalculatorBrain.swift
@@ -17,12 +17,35 @@ struct CalculatorBrain {
     //classes live in the heap, structs are not (copied by value)
     
     //internal var
+    let maxValue = Int.max
     private var accumulator: Double?
+    private var resultIsPending = false
+    
+    
+    var description: String?{
+        didSet {
+            if pendingBinaryOperation == nil {
+                currOrder = maxValue
+            }
+        }
+    }
+    var accumulatedDescription:String?
+    //var accumulatedDescription: String {
+      //  get {
+       //     if pendingBinaryOperation == nil {
+       //         return description
+       //     } else {
+       //         return pendingDescription!.functionDescription(String(pendingDescription!.firstOperand), String(pendingDescription!.firstOperand) != description ? /description : "")
+       //     }
+       // }
+    //}
+
+    private var currOrder = Int.max
     
     private enum Operation{
         case constant(Double) //assciated value - just like optional, which is also an enum
-        case unaryOperation((Double)->Double)
-        case binaryOperation((Double,Double)->Double)
+        case unaryOperation((Double)->Double, (String) -> String)
+        case binaryOperation((Double,Double)->Double, (String, String) -> String, Int)
         case equals
         
     }
@@ -30,57 +53,103 @@ struct CalculatorBrain {
     private var operations: Dictionary<String, Operation> = [
     "π": Operation.constant(Double.pi),
     "e": Operation.constant(M_E),
-    "√": Operation.unaryOperation(sqrt),
-    "cos": Operation.unaryOperation(cos),
-    "±": Operation.unaryOperation({-$0}),
-    "x": Operation.binaryOperation({$0 * $1}),
-    "+": Operation.binaryOperation({$0 + $1}),
-    "-": Operation.binaryOperation({$0 - $1}),
-    "÷": Operation.binaryOperation({$0 / $1}),
-    "sin": Operation.unaryOperation(sin),
-    "tan": Operation.unaryOperation(tan),
-    "x²": Operation.unaryOperation({pow($0, 2)}),
-    "x³": Operation.unaryOperation({pow($0, 3)}),
-    "xʸ": Operation.binaryOperation(pow),
+    "√": Operation.unaryOperation(sqrt, { "√(\($0))"} ),
+    "cos": Operation.unaryOperation(cos, {"cos(\($0))"}),
+    "±": Operation.unaryOperation({-$0}, {"-(\($0))"}),
+    "x": Operation.binaryOperation({$0 * $1}, { "\($0) x \($1)"}, 1),
+    "+": Operation.binaryOperation({$0 + $1}, { "\($0) + \($1)"}, 1),
+    "-": Operation.binaryOperation({$0 - $1}, { "\($0) - \($1)"}, 0),
+    "÷": Operation.binaryOperation({$0 / $1}, { "\($0) ÷ \($1)"}, 0),
+    "sin": Operation.unaryOperation(sin, {"sin(\($0))"}),
+    "tan": Operation.unaryOperation(tan, {"tan(\($0))"}),
+    "x²": Operation.unaryOperation({pow($0, 2)}, {"(\($0))²"}),
+    "x³": Operation.unaryOperation({pow($0, 3)}, {"(\($0))³"}),
+    "xʸ": Operation.binaryOperation(pow, { "\($0) ^ \($1)"}, 2),
     "=": Operation.equals
     ]
     mutating func performOperation (_ symbol: String){
         if let operation = operations[symbol]{
             switch operation{
             case .constant(let value):
+                
                 accumulator = value
-            case .unaryOperation(let function):
+                description = symbol
+                //performPendingDescription()
+                
+            case .unaryOperation(let function, let functionDescription):
+                
                 if accumulator != nil {
                     accumulator = function(accumulator!)
+                    
+                    
                 }
-            case .binaryOperation(let function):
+                description = functionDescription(description!)
+                //performPendingDescription()
+            case .binaryOperation(let function, let functionDescription, let order):
+                performPendingBinaryOperation()
+                //performPendingDescription()
+                if order > currOrder  {
+                    description = "(" + description! + ")"
+                }
+                //description = functionDescription(description)
+                currOrder = order
+                pendingDescription = PendingDescription(currDescription: description!,functionDescription: functionDescription)
                 if accumulator != nil{
                     pendingBinaryOperation = PendingBinaryOperation(function: function, firstOperand: accumulator!)
+                    
+                    
+                    
                     accumulator = nil
                 }
                 
             case .equals:
                 performPendingBinaryOperation()
+                //performPendingDescription()
                 
             }
             
             
         }
     }
+
     
     private mutating func performPendingBinaryOperation(){
-        if pendingBinaryOperation != nil && accumulator != nil {
-            accumulator = pendingBinaryOperation!.perform(with: accumulator!)
-            pendingBinaryOperation = nil
+        if pendingDescription != nil {
+            description = pendingDescription!.perform(with: description!)
+            pendingDescription = nil
             
         }
+        if pendingBinaryOperation != nil && accumulator != nil {
+            accumulator = pendingBinaryOperation!.perform(with: accumulator!)
+          
+            pendingBinaryOperation = nil
+            
+            
+        }
+        
+
     }
     private var pendingBinaryOperation: PendingBinaryOperation?
+    private var pendingDescription: PendingDescription?
+    
+     private struct PendingDescription {
+        let currDescription: String
+        //let firstOperand: Double
+        let functionDescription: (String, String) -> String
+        func perform(with secondOperand: String) -> String{
+            //description = functionDescription(firstOperand, secondOperand)
+
+                return functionDescription(currDescription, String(secondOperand))
+            
+
+        }
+    }
     private struct PendingBinaryOperation{
         let function: (Double,Double) -> Double
         let firstOperand: Double
-        
+ 
         func perform(with secondOperand: Double) -> Double{
+            //description = functionDescription(firstOperand, secondOperand)
             return function(firstOperand, secondOperand)
         }
         
@@ -88,6 +157,7 @@ struct CalculatorBrain {
     
     mutating func setOperand(_ operand: Double) {
         accumulator = operand
+        description = String(operand)
     }
     
     var result: Double? {
--- Calculator/ViewController.swift
+++ Calculator/ViewController.swift
@@ -11,23 +11,28 @@ import UIKit
 class ViewController: UIViewController {
     
     @IBOutlet weak var display: UILabel!
+    @IBOutlet weak var descriptionDisplay: UILabel!
     
     var userIsInTheMiddleOfTyping: Bool = false
     //var userIsInTheMiddleOfTypingDecimal:Bool = false
     
     @IBAction func touchDigit(_ sender: UIButton) {
         let digit = sender.currentTitle!
+        
         if userIsInTheMiddleOfTyping {
             if display.text!.contains(".") && digit == "."{
                 return
             }
             let textCurrentlyInDisplay = display.text!
             display.text = textCurrentlyInDisplay + digit
+            descriptionDisplay.text = descriptionDisplay.text! + digit
         } else {
             if digit == "."  {
                 display.text = "0" + digit
+                descriptionDisplay.text = "0" + digit
             } else {
                 display.text = digit
+                descriptionDisplay.text = digit
             }
             userIsInTheMiddleOfTyping = true
         }
@@ -43,6 +48,7 @@ class ViewController: UIViewController {
         set{
             //newValue ex - if we use displayValue = 5, then newValue is 5
             display.text = String(newValue)
+            descriptionDisplay.text = brain.description
         }
     }
     
@@ -54,6 +60,7 @@ class ViewController: UIViewController {
             brain.setOperand(displayValue)
             userIsInTheMiddleOfTyping = false
         }
+        //descriptionDisplay.text = brain.description
         //sets mathemcaticalSymbol if we can unwrap sender.currentTitle
         if let mathematicalSymbol = sender.currentTitle {
             brain.performOperation(mathematicalSymbol)
@@ -61,6 +68,7 @@ class ViewController: UIViewController {
         if let result = brain.result {
             displayValue = result
         }
+        //descriptionDisplay.text = brain.description
     }
 
 }
